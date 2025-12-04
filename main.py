582692007ca77981a9e40f002a87c4137d61e795
rhfill <junjia7673@gmail.com>
2025-12-04 15:28:00 -0800

    New endpoint: return current month

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from datetime import datetime
 app = FastAPI()
 
 @app.get("/year")
-async def root():
+async def get_current_year():
     """Returns the current year."""
     current_year = datetime.now().year
     return {"year": current_year}
+
+@app.get("/month")
+async def get_current_month():
+    """Returns the current month."""
+    current_month = datetime.now().month
+    return {"month": current_month}
 
